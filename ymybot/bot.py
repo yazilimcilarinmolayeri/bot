c2ed647cfab1c8ebb79e5dc0e9f58f8e2d6465f5
ymy-git <ymy.git@protonmail.com>
2020-01-04 13:33:41 +0300

    bazı ufak düzeltmeler

--- ymybot/bot.py
+++ ymybot/bot.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019, Yazılımcıların Mola Yeri (ymy-gitrepo)
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# Please read the COPYING file.
+# Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymydepo)
 #
 
 import config
