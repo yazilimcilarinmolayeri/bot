1bdfd142c583da9d4e499a2be315f8c05be742ce
ymy-git <ymy.git@protonmail.com>
2020-01-05 14:52:17 +0300

    version şuan karıştı neyse çaktırma

--- ymybot/__init__.py
+++ ymybot/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymydepo)
 #
 
-__version__ = "1.0.1"
+__version__ = "0.1.2"
 __state__ = "Pre-Alpha"
 __author__ = "Yazılımcıların Mola Yeri"
 __license__ = "GNU General Public License v3.0"
