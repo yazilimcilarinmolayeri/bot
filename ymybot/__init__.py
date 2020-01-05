a672cc391bbec7dd2c145e6ffa2184aa5dc37521
ymy-git <ymy.git@protonmail.com>
2020-01-05 13:20:09 +0300

    ufak tefek değişiklikler...

--- ymybot/__init__.py
+++ ymybot/__init__.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019, Yazılımcıların Mola Yeri (ymy-gitrepo)
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# Please read the COPYING file.
+# Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymydepo)
 #
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __state__ = "Pre-Alpha"
 __author__ = "Yazılımcıların Mola Yeri"
 __license__ = "GNU General Public License v3.0"
--- ymybot/cogs/utils/emoji.py
+++ ymybot/cogs/utils/emoji.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019, Yazılımcıların Mola Yeri (ymy-gitrepo)
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# Please read the COPYING file.
+# Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymydepo)
 #
 
 import emoji
