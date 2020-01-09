db08a73a6a698d4383a85315f7e56346a11ccf2a
ymy-git <ymy.git@protonmail.com>
2020-01-09 20:25:19 +0300

    2 yeni komut eklendi

--- ymybot/cogs/mod.py
+++ ymybot/cogs/mod.py
@@ -3,6 +3,8 @@
 # Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymydepo)
 #
 
+from cogs.utils import checks
+
 from discord.ext import commands
 import discord
 
@@ -12,6 +14,26 @@ class Mod(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+    
+    @checks.is_mod()
+    @commands.command(aliases=["süpür"])
+    async def clean(self, ctx, amount=1):
+        channel = ctx.message.channel
+
+        messages = list()
+        # Verdiğimiz komutda bir mesaj olduğu için fazladan 1 tane daha siliyoruz.
+        async for message in channel.history(limit=int(amount) + 1):
+            messages.append(message)
+
+        await channel.delete_messages(messages)
+        await channel.send(
+            f"`{len(messages)}` mesaj başarıyla süpürüldü!", delete_after=3,
+        )
+    
+    @checks.is_mod()
+    @commands.command(aliases=["yazdır"])
+    async def print(self, ctx, channel: discord.TextChannel, *, content):
+        await channel.send(content)
 
 
 def setup(bot):
