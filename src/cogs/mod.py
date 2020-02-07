8945396b2fa2f8769a40b593c8ed8156a9b005b8
ymypengueni <ymy.git@protonmail.com>
2020-02-08 01:08:22 +0300

    komut izinleri ayarlandı

--- src/cogs/mod.py
+++ src/cogs/mod.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019-2020, Yazılımcıların Mola Yeri (ymy-discord)
-#
 
-from cogs.utils import checks
+from .utils import checks
 
 import discord
 from discord.ext import commands
@@ -15,10 +13,12 @@ class Mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @checks.is_mod()
+    @commands.guild_only()
     @commands.command(aliases=["süpür"])
+    @checks.has_permissions(manage_messages=True)
     async def clean(self, ctx, amount: int = 1):
-        """ Belirlenen sayıda mesajı temizler """
+        """Kanaldaki mesajları verilen miktar kadar temizler."""
+
         channel = ctx.message.channel
 
         messages = list()
@@ -27,14 +27,14 @@ class Mod(commands.Cog):
             messages.append(message)
 
         await channel.delete_messages(messages)
-        await channel.send(
-            f"`{len(messages)-1}` mesaj başarıyla süpürüldü!", delete_after=3,
-        )
+        await channel.send(f"`{len(messages)-1}` mesaj süpürüldü!", delete_after=3)
 
-    @checks.is_mod()
+    @commands.guild_only()
     @commands.command(aliases=["yazdır"])
+    @checks.is_mod()
     async def echo(self, ctx, channel: discord.TextChannel, *, content: str):
-        """ Botun belirlenen kanala mesaj göndermesini sağlar """
+        """Belirlenen kanala mesaj içeriğini gönderir."""
+
         await channel.send(content)
 
 
