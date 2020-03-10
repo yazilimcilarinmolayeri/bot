7eaf4b805a40354057b2007637f7b4a6608cd3cd
ymypengueni <ymy.git@protonmail.com>
2020-03-10 21:51:31 +0300

    süpür komutu yenilendi

--- src/cogs/mod.py
+++ src/cogs/mod.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 
+import json
+
 from .utils import checks
 
 import discord
@@ -16,26 +18,32 @@ class Mod(commands.Cog):
     @commands.guild_only()
     @commands.command(aliases=["süpür"])
     @checks.has_permissions(manage_messages=True)
-    async def clean(self, ctx, amount: int = 1):
+    async def clean(self, ctx, amount: int):
         """Kanaldaki mesajları verilen miktar kadar temizler."""
 
         channel = ctx.message.channel
-
-        messages = list()
         # Verdiğimiz komutda bir mesaj olduğu için fazladan 1 tane daha siliyoruz.
-        async for message in channel.history(limit=int(amount) + 1):
-            messages.append(message)
+        deleted = await channel.purge(limit=amount + 1)
 
-        await channel.delete_messages(messages)
-        await channel.send(f"`{len(messages)-1}` mesaj süpürüldü!", delete_after=3)
+        await ctx.send(f"`{len(deleted)-1}` mesaj süpürüldü.", delete_after=3.0)
 
     @commands.guild_only()
-    @commands.command(aliases=["yazdır"])
+    @commands.group(aliases=["yazdır"], invoke_without_command=True)
     @checks.is_mod()
     async def echo(self, ctx, channel: discord.TextChannel, *, content: str):
         """Belirlenen kanala mesaj içeriğini gönderir."""
 
         await channel.send(content)
+
+    @echo.command()
+    async def embed(self, ctx, channel: discord.TextChannel, *, content):
+        """
+        JSON veri formatında verilen içeriği embed olarak gönderir.
+        İçerik üretmek için: https://bastion.traction.one/embedbuilder/
+        """
+
+        embed = discord.Embed.from_dict(json.loads(content))
+        await channel.send(embed=embed)
 
 
 def setup(bot):
