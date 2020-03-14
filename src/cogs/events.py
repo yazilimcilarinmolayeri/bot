714f920fcb7d03551d9ab6db51a2401cfb85ebce
ymypengueni <ymy.git@protonmail.com>
2020-03-15 01:48:10 +0300

    olaylar yeniden ReactionRole sınıfına göre yeniden düzenlendi

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -2,12 +2,11 @@
 #
 
 import sys
-import config
 import traceback
 
+import config
 from .utils import meta
-from .utils import role
-from .utils import emoji as e
+from .utils.rr import ReactionRole
 
 import discord
 from discord.utils import get
@@ -20,15 +19,6 @@ class Events(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    def get_emoji(self, emoji):
-        if emoji.id == None:
-            return e.unicode_to_name(emoji)
-        else:
-            return emoji.id
-
-    def get_role(self, guild, emoji):
-        return get(guild.roles, name=role.roles[str(emoji)])
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, err):
@@ -104,57 +94,20 @@ class Events(commands.Cog):
 
             return await mentionlog.send(embed=embed)
 
-    # Bu kodlara el atılacak. Asıl fonksiyonlar utils altına alınacak.
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         guild_id = payload.guild_id
         channel_id = payload.channel_id
-        message_id = payload.message_id
-        user_id = payload.user_id
-        emoji = payload.emoji
-
+        
+        reaction_role = ReactionRole(self.bot, payload)
+        
         if guild_id == config.ymy_guild_id:
-            if (
-                channel_id == config.reaction_role_channel_id
-                and message_id in config.reaction_role_message_ids
-            ):
-                guild = self.bot.get_guild(id=guild_id)
-                user = guild.get_member(user_id)
-
-                try:
-                    role = self.get_role(guild, self.get_emoji(emoji))
-                    await user.add_roles(role)
-                    await user.send(f"`{role}` rolü alındı.")
-
-                except KeyError:
-                    channel = self.bot.get_channel(channel_id)
-                    message = await channel.fetch_message(message_id)
-                    await message.remove_reaction(emoji=emoji, member=user)
+            if channel_id == config.reaction_role_channel_id:
+                await reaction_role.add_or_remove()
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        guild_id = payload.guild_id
-        channel_id = payload.channel_id
-        message_id = payload.message_id
-        user_id = payload.user_id
-        emoji = payload.emoji
-
-        if guild_id == config.ymy_guild_id:
-            if (
-                channel_id == config.reaction_role_channel_id
-                and message_id in config.reaction_role_message_ids
-            ):
-                guild = self.bot.get_guild(id=guild_id)
-                user = guild.get_member(user_id)
-
-                try:
-                    role = self.get_role(guild, self.get_emoji(emoji))
-                    await user.remove_roles(role)
-                    await user.send(f"`{role}` rolü bırakıldı.")
-
-                except KeyError:
-                    pass
+        pass
 
 
 def setup(bot):
