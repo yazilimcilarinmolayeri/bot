d1ccd1a4130ac9fba1f316a2322edad1d089dc50
ymypengueni <ymy.git@protonmail.com>
2020-03-18 21:29:11 +0300

    update_activity_name fonksiyonu güncellendi

--- src/cogs/utils/meta.py
+++ src/cogs/utils/meta.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 #
+# Yardımcı fonksiyonlar...
+#
+
+from collections import Counter
 
 import config
 
@@ -10,16 +14,12 @@ async def update_activity_name(bot):
     """Aktivite kısmına insan kullanıcı sayısını yazdırır."""
 
     guild = bot.get_guild(config.ymy_guild_id)
-
-    b = 0
-    for m in guild.members:
-        if m.bot == True:
-            b += 1
-
-    members = len(guild.members) - b
+    # Bot miktarı True, bot olmayanların miktarı False anahtar isminde sayılır.
+    # {"True": 0, "False": 0}
+    human = Counter(str(m.bot) for m in guild.members)["False"]
 
     await bot.change_presence(
         activity=discord.Activity(
-            type=discord.ActivityType.watching, name=f"{members:,d} üyeyi"
+            type=discord.ActivityType.watching, name=f"{human:,d} üyeyi"
         )
     )
