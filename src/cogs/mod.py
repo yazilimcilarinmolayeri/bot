0fbb8896050c86b1aa427ab1aac9515b1e5ffb3d
ymypengueni <ymy.git@protonmail.com>
2020-03-22 01:05:42 +0300

    dm ve placerr komutu eklendi

--- src/cogs/mod.py
+++ src/cogs/mod.py
@@ -2,7 +2,10 @@
 #
 
 import json
+import string
+import emoji as emo
 
+import config
 from .utils import checks
 
 import discord
@@ -45,6 +48,53 @@ class Mod(commands.Cog, name="Moderation"):
         embed = discord.Embed.from_dict(json.loads(content))
         await channel.send(embed=embed)
 
+    @echo.command()
+    async def dm(self, ctx, user_id, *, content):
+        """Kimliği verilen kullanıcıya verilen içeriği doğrudan gönderir."""
+        
+        user = await self.bot.fetch_user(user_id)
+        await user.send(content)
+        
+        dmlog = self.bot.get_channel(687804890860486762)
+        embed = discord.Embed(color=discord.Colour.green())
+        embed.description = content
+        embed.set_author(name=user, icon_url=user.avatar_url)
+        embed.set_footer(text=f"ID: {user.id}")
 
+        await dmlog.send(embed=embed)
+
+    @commands.guild_only()
+    @commands.command()
+    @checks.is_mod()
+    async def placerr(self, ctx):
+        """
+        Deneyseldir. data/rr_data.json dosyasındaki verileri kullanrak reaksiyon rol 
+        mesajlarını günceller.
+        """
+        
+        with open("src/cogs/utils/data/rr_data.json") as f:
+            rr_data = json.load(f)
+        
+        channel = self.bot.get_channel(config.rr_channel_id)
+        
+        def get_unicode(name):
+            return emo.emojize(f":{name}:")
+        
+        for message_id in rr_data.keys():
+            rr = rr_data[message_id]["rr"]
+            
+            embed = discord.Embed(color=self.bot.embed_color)
+            embed.title = rr_data[message_id]["title"]
+            embed.description = "\n".join([f"{get_unicode(i)} : {rr[i]}" for i in rr])
+            
+            message = await channel.fetch_message(message_id)
+            await message.edit(embed=embed)
+            
+            for i in rr.keys():
+                await message.add_reaction(get_unicode(i))
+        
+        await ctx.message.add_reaction("\u2705")
+    
+        
 def setup(bot):
     bot.add_cog(Mod(bot))
