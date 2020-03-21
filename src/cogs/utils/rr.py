a41cb01ee2514f2c764e9a11f5547203f9d5b8a3
ymypengueni <ymy.git@protonmail.com>
2020-03-22 00:50:07 +0300

    Reaksiyon rol olayları güncellendi

--- src/cogs/utils/rr.py
+++ src/cogs/utils/rr.py
@@ -4,9 +4,9 @@
 # Reaksiyon Rol Sistemi
 #
 
-import emoji as emo
+import json
 
-from cogs.utils import role
+import emoji as emo
 
 import discord
 from discord.utils import get
@@ -21,6 +21,13 @@ class ReactionRole:
         self.emoji = payload.emoji
         self.guild = self.bot.get_guild(id=payload.guild_id)
         self.member = self.guild.get_member(payload.user_id)
+        self.get_data()
+
+    def get_data(self):
+        with open("src/cogs/utils/data/rr_data.json") as f:
+            rr_data = json.load(f)
+
+        self.rr_data = rr_data
 
     def unicode_to_shortcode(self, unicode_emoji):
         """Unicode olarak verilen emojinin adını döndürür."""
@@ -33,21 +40,23 @@ class ReactionRole:
         if emoji.is_unicode_emoji():
             return self.unicode_to_shortcode(emoji)
         else:
-            return emoji.id
+            pass
 
     def get_role(self, guild, emoji):
         """Reaksiyona eklelen emojiye karşılık gelen rolü döner."""
 
-        return get(guild.roles, name=role.roles[str(emoji)])
+        return get(
+            guild.roles,
+            name=self.rr_data[str(self.payload.message_id)]["rr"][emoji[1:-1]],
+        )
 
     def role_check(self):
         """Reaksiyona karşılık gelen rolün kullanıcıda olup olmadığına bakar."""
 
-        member_roles = self.member.roles
         self.role = self.get_role(self.guild, self.get_emoji(self.emoji))
 
         status = False
-        for mr in member_roles:
+        for mr in self.member.roles:
             if mr.id == self.role.id:
                 status = True
                 break
@@ -65,19 +74,23 @@ class ReactionRole:
 
     async def add_role(self):
         """Kullanıcıya rol ekler. Mesaj gönderir. Eklediği tepkiyi siler."""
+        
+        # Eğer rol sayısı 20'ye eşit yada fazla ise kullanıcı daha fazla rol alamaz.
+        if len(self.member.roles) - 1 >= 20:
+            await self.remove_reaction()
+            await self.member.send(f"\N{NO ENTRY} En fazla 20 rol alabilirsin!")
+            return
 
         await self.remove_reaction()
         await self.member.add_roles(self.role)
-        await self.member.send("\N{INBOX TRAY} "
-                               f"`{self.role.name}` rolü eklendi!")
+        await self.member.send(f"\N{INBOX TRAY} `{self.role.name}` rolü eklendi!")
 
     async def remove_role(self):
         """Kullanıcıdan rol siler. Mesaj gönderir. Eklediği tepkiyi siler."""
 
         await self.remove_reaction()
         await self.member.remove_roles(self.role)
-        await self.member.send("\N{OUTBOX TRAY} "
-                               f"`{self.role.name}` rolü silindi!")
+        await self.member.send(f"\N{OUTBOX TRAY} `{self.role.name}` rolü silindi!")
 
     async def add_or_remove(self):
         """Eklenen tepkiye göre kullanıcının rolü yok ise ekler, var ise siler."""
